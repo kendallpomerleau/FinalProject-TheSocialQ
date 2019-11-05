15c6533e8121a675debadd637d9eb0528f616943
Sarah Chitty <sarahchitty@wustl.edu>
2019-11-05 17:06:03 -0600

    play song but merge not working

---
+++ FinalProject-TheSocialQ/HostQueueViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HostQueueViewController.swift
+//  FinalProject-TheSocialQ
+//
+//  Created by Sarah Chitty on 11/5/19.
+//  Copyright © 2019 Kendall Pomerleau. All rights reserved.
+//
+
+import UIKit
+
+class HostQueueViewController: UIViewController {
+
+    @IBOutlet weak var playSongView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.playSongView.layer.cornerRadius = 9
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
