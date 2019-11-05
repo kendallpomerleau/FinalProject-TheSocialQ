2d0e89bee35c125b41ada6521af15bed43bad9de
Kendall Pomerleau <kendallpomerleau@gmail.com>
2019-11-04 20:28:33 -0600

    first page done!

--- FinalProject-TheSocialQ/ViewController.swift
+++ FinalProject-TheSocialQ/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hostQueueBtn: UIButton!
+    @IBOutlet weak var joinQueueBtn: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        hostQueueBtn.layer.cornerRadius = 10
+        hostQueueBtn.clipsToBounds = true
+        
+        joinQueueBtn.layer.cornerRadius = 10
+        joinQueueBtn.clipsToBounds = true
+        
         // Do any additional setup after loading the view.
     }
 
