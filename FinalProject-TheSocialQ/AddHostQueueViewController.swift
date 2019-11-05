36d1ec73bdb79b4b6e923ca65b31ccf7fb1878f6
Sarah Chitty <sarahchitty@wustl.edu>
2019-11-05 12:44:07 -0600

    added view controller for host queue info and added elements

---
+++ FinalProject-TheSocialQ/AddHostQueueViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddHostQueueViewController.swift
+//  FinalProject-TheSocialQ
+//
+//  Created by Sarah Chitty on 11/5/19.
+//  Copyright © 2019 Kendall Pomerleau. All rights reserved.
+//
+
+import UIKit
+
+class AddHostQueueViewController: UIViewController {
+
+    @IBOutlet weak var Access: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
