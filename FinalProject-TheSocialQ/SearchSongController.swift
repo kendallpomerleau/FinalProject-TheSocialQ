5558c9a665d5cb70ec285bf9628a48761dbf498c
Kendall Pomerleau <kendallpomerleau@gmail.com>
2019-11-06 10:47:28 -0600

    worked on joining queue functionality

--- FinalProject-TheSocialQ/SearchSongController.swift
+++ FinalProject-TheSocialQ/SearchSongController.swift
@@ -8,19 +8,51 @@
 
 import UIKit
 
-class SearchSongController: UIViewController {
+class SearchSongController: UIViewController, UITableViewDataSource, UITabBarDelegate {
+    
     
     var songResults:[Song] = []
+    var imageCache:[UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // INITIAL SONGS JUST FOR TESTING
-        let circles = Song(id: 1, title: "Circles")
-        let cyanide = Song(id: 2, title: "Cyanide")
+        cacheImages()
         
-        songResults.append(circles)
-        songResults.append(cyanide)
+    }
+    
+    func cacheImages() {
+        imageCache = []
+        for song in songResults {
+            if song.coverPath != nil {
+                let url = URL(string: song.coverPath!)
+                let data = try? Data(contentsOf: url!)
+                if (data != nil){
+                    let image = UIImage(data:data!)
+                    imageCache.append(image!)
+                }
+            }
+            else {
+                // append empty image
+                imageCache.append(UIImage())
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return songResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        
+        cell.textLabel!.text = songResults[indexPath.row].title
+        cell.detailTextLabel?.text = songResults[indexPath.row].artist
+        cell.imageView?.image = imageCache[indexPath.row]
+        
+        return cell
     }
     
 
